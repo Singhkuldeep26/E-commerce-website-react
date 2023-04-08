dc5b0bed118392a200b326db733bf4ca0960cd64
kuldeep singh <pssingh1145@gmail.com>
2023-04-09 01:17:22 +0530

    Update createStore

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import './index.css';
 import App from './App';
 import products from './reducers/index'
 import "@mui/material"
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'
-const store=createStore(products)
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+const store = configureStore({ reducer: products })
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-
         <App />
     </Provider>
 );
